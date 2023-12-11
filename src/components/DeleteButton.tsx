8af6802f4c2fcaedea01ca281b228978bb8567c8
Adithya Arunganesh <adithyaa@Adithyas-MBP.attlocal.net>
2023-12-11 16:24:41 -0600

    firebase storage setup

---
+++ src/components/DeleteButton.tsx
@@ -0,0 +1,43 @@
+'use client'
+import React from 'react'
+import { Button } from './ui/button'
+import { Trash } from 'lucide-react'
+import { useRouter } from 'next/navigation'
+import { useMutation } from '@tanstack/react-query'
+import axios from 'axios'
+
+type Props = {
+    noteId: number
+}
+
+const DeleteButton = ({noteId}: Props) => {
+    const router = useRouter()
+    const deleteNote = useMutation({
+        mutationFn: async () => {
+            const response = await axios.post('/api/deleteNote', {
+                noteId
+            })
+            return response.data
+        }
+    })
+  return (
+    <Button variant={'destructive'} size='sm' disabled={deleteNote.isPending} onClick={()=>{
+        const confirm = window.confirm('Are you sure you want to delete this note?')
+        if (!confirm) {
+            return;
+        }
+        deleteNote.mutate(undefined, {
+            onSuccess: () => {
+                router.push('/dashboard')
+            },
+            onError: (err) => {
+                console.error(err)
+            }
+        })
+    }}>
+        <Trash></Trash>
+    </Button>
+  )
+}
+
+export default DeleteButton
